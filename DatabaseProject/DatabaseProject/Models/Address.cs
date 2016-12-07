7dc1c9860def3f5ac22577c557a04bd89035c80c
shariqxmohammad <shariqxmohammad@gmail.com>
2016-12-07 01:57:26 -0600

    Update Address.cs

--- DatabaseProject/DatabaseProject/Models/Address.cs
+++ DatabaseProject/DatabaseProject/Models/Address.cs
@@ -2,7 +2,7 @@
 {
     public class Address
     {
-        public string Street { get; set; }
+        public string Street { get; set; } 
         public string City { get; set; }
         public string State { get; set; }
     }
